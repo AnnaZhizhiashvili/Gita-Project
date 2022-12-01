49187c3b0e2cf525d1a25ea18da3743741895b0a
AnZhizhiashvili <anzhizhiashvili@tbcbank.com.ge>
2022-12-01 22:48:01 +0400

    add background image, make first api call, add sub-categories

--- app.js
+++ app.js
@@ -1,6 +1,11 @@
-import { ana } from "./server.js";
+import { getData } from "./server.js";
 const hamburger = document.querySelector(".hamburger");
 const navMenuSmall = document.querySelector(".navbar.small-res");
+const background = document.querySelector(".background-img");
+const newsHeader = document.querySelector(".background-img h2");
+const newsDescription = document.querySelector(".background-img p");
+const categories = document.querySelector(".categories");
+const subCategories = document.querySelector(".sub-categories");
 
 hamburger.addEventListener("click", () => {
   hamburger.classList.toggle("active");
@@ -8,6 +13,36 @@ hamburger.addEventListener("click", () => {
 });
 
 window.addEventListener("resize", (event) => {
-  hamburger.classList.remove("active");
-  navMenuSmall.classList.remove("active");
+  subCategories.classList.remove("visible");
+  if (window.innerWidth > 768) {
+    hamburger.classList.remove("active");
+    navMenuSmall.classList.remove("active");
+  }
 });
+
+document.addEventListener("click", function (e) {
+  var el = e.target;
+  if (!categories.contains(el)) {
+    subCategories.classList.remove("visible");
+  }
+});
+
+categories.addEventListener("click", () => {
+  subCategories.classList.toggle("visible");
+});
+
+const data = await getData();
+let i = 0;
+const changeBackgroundNews = () => {
+  background.style.backgroundImage = `url("../assets/images/background-images/${i}.jpg")`;
+  newsHeader.innerHTML = data[i].title;
+  newsDescription.innerHTML = data[i].description;
+  i++;
+  if (i > 3) {
+    i = 0;
+  }
+};
+
+console.log(data);
+
+setInterval(changeBackgroundNews, 2000);
--- server.js
+++ server.js
@@ -3,20 +3,16 @@ const apiKey = "3eaefda756963830cca66705c8189ef4";
 const ROOT_URL = "http://api.mediastack.com/v1/news";
 
 const urls = {
-  all: `${ROOT_URL}?access_key=${apiKey}`,
+  all: `${ROOT_URL}?access_key=${apiKey}&languages=en`,
 };
 
-export function ana() {
-  //   fetch(urls.all)
-  //     .then((response) => response.json())
-  //     .then((news) => {
-  //       console.log(news);
-  //       console.log(document.querySelector("h1"));
-  //       document.querySelector(
-  //         "h1"
-  //       ).innerHTML = `<img src='${news.data[5].image}'>`;
-  //     })
-  //     .catch((error) => {
-  //       console.log(error);
-  //     });
+export async function getData() {
+  let resJson;
+  try {
+    const res = await fetch(urls.all);
+    resJson = await res.json();
+  } catch (err) {
+    console.log(err);
+  }
+  return resJson.data;
 }
